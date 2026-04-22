6b3ef9dad7461e6a68cb770ad7fe9f7954d1b06b
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:02:30 -0500

    TUnit conversion

--- test/Soenneker.Quark.Gen.Lucide.Tests/LucideGeneratorTests.cs
+++ test/Soenneker.Quark.Gen.Lucide.Tests/LucideGeneratorTests.cs
@@ -1,11 +1,10 @@
 using Soenneker.Tests.Unit;
-using Xunit;
 
 namespace Soenneker.Quark.Gen.Lucide.Tests;
 
 public sealed class LucideGeneratorTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
